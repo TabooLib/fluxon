04ddbd289512475795a385d689c91cc6d25b1d85
黑 <b@taboo.sh>
2025-08-20 06:51:04 +0800

    修复 return 语句的字节码生成逻辑

--- core/src/main/java/org/tabooproject/fluxon/interpreter/evaluator/stmt/ReturnEvaluator.java
+++ core/src/main/java/org/tabooproject/fluxon/interpreter/evaluator/stmt/ReturnEvaluator.java
@@ -4,12 +4,15 @@ import org.objectweb.asm.MethodVisitor;
 import org.tabooproject.fluxon.interpreter.Interpreter;
 import org.tabooproject.fluxon.interpreter.ReturnValue;
 import org.tabooproject.fluxon.interpreter.bytecode.CodeContext;
+import org.tabooproject.fluxon.interpreter.error.EvaluatorNotFoundException;
+import org.tabooproject.fluxon.interpreter.evaluator.Evaluator;
 import org.tabooproject.fluxon.interpreter.evaluator.StatementEvaluator;
+import org.tabooproject.fluxon.parser.ParseResult;
 import org.tabooproject.fluxon.parser.statement.ReturnStatement;
 import org.tabooproject.fluxon.parser.statement.StatementType;
 import org.tabooproject.fluxon.runtime.Type;
 
-import static org.objectweb.asm.Opcodes.ATHROW;
+import static org.objectweb.asm.Opcodes.*;
 
 public class ReturnEvaluator extends StatementEvaluator<ReturnStatement> {
 
@@ -30,8 +33,27 @@ public class ReturnEvaluator extends StatementEvaluator<ReturnStatement> {
 
     @Override
     public Type generateBytecode(ReturnStatement result, CodeContext ctx, MethodVisitor mv) {
-        // 生成字节码
-        mv.visitInsn(ATHROW);
+        // 处理返回值
+        if (result.getValue() != null) {
+            // 有返回值：计算表达式并返回
+            Evaluator<ParseResult> valueEval = ctx.getEvaluator(result.getValue());
+            if (valueEval == null) {
+                throw new EvaluatorNotFoundException("No evaluator found for return value expression");
+            }
+            // 生成返回值表达式的字节码
+            Type valueType = valueEval.generateBytecode(result.getValue(), ctx, mv);
+            // 如果表达式返回 void，则返回 null
+            if (valueType == Type.VOID) {
+                mv.visitInsn(ACONST_NULL);
+            }
+            // 返回对象引用
+            mv.visitInsn(ARETURN);
+        } else {
+            // 无返回值：返回 null
+            mv.visitInsn(ACONST_NULL);
+            mv.visitInsn(ARETURN);
+        }
+        // Return 语句本身不返回值
         return Type.VOID;
     }
 }
--- core/src/test/fs/test5.fs
+++ core/src/test/fs/test5.fs
@@ -1,6 +1,10 @@
-find = forName("org.tabooproject.fluxon.runtime.FluxonRuntime")
-print &find
-print &find :: getDeclaredField("INSTANCE") :: get &find
+def instance(name) = {
+    find = forName(&name)
+    return &find :: getDeclaredField("INSTANCE") :: get(&find)
+}
 
+// 输出扩展函数
 print(env::rootExtensionFunctions()::keySet()::size())
 print(env::rootExtensionFunctions()::keySet())
+
+return instance("org.tabooproject.fluxon.runtime.FluxonRuntime")
