eccf5d8f2ffbff3435eaeb61b1c8d840936c52d8
黑 <b@taboo.sh>
2025-11-15 13:57:56 +0800

    feat(build): 更新主类为 FluxonConsole，优化运行配置

--- core-console/build.gradle.kts
+++ core-console/build.gradle.kts
@@ -1,3 +1,9 @@
+import org.gradle.api.tasks.JavaExec
+
+application {
+    mainClass.set("org.tabooproject.fluxon.FluxonConsole")
+}
+
 dependencies {
     implementation(project(":core"))
     implementation(project(":core-jsr223"))
@@ -6,6 +12,10 @@ dependencies {
 tasks.jar {
     archiveBaseName = "fluxon-core-console"
     manifest {
-        attributes["Main-Class"] = "org.tabooproject.fluxon.FluxonLine"
+        attributes["Main-Class"] = "org.tabooproject.fluxon.FluxonConsole"
     }
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
