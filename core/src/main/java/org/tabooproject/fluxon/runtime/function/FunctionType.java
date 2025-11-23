98ccefa711ce3253448433d701f343aba02abfa8
黑 <b@taboo.sh>
2025-11-24 00:36:28 +0800

    feat(function): 添加可返回 null 的类型转换函数

--- core/src/main/java/org/tabooproject/fluxon/runtime/function/FunctionType.java
+++ core/src/main/java/org/tabooproject/fluxon/runtime/function/FunctionType.java
@@ -9,9 +9,17 @@ public class FunctionType {
 
     public static void init(FluxonRuntime runtime) {
         runtime.registerFunction("int", 1, (context) -> Coerce.asInteger(context.getArgument(0)).orElse(0));
+        runtime.registerFunction("intOrNull", 1, (context) -> Coerce.asInteger(context.getArgument(0)).orElse(null));
+
         runtime.registerFunction("long", 1, (context) -> Coerce.asLong(context.getArgument(0)).orElse(0L));
+        runtime.registerFunction("longOrNull", 1, (context) -> Coerce.asLong(context.getArgument(0)).orElse(null));
+
         runtime.registerFunction("float", 1, (context) -> Coerce.asFloat(context.getArgument(0)).orElse(0f));
+        runtime.registerFunction("floatOrNull", 1, (context) -> Coerce.asFloat(context.getArgument(0)).orElse(null));
+
         runtime.registerFunction("double", 1, (context) -> Coerce.asDouble(context.getArgument(0)).orElse(0d));
+        runtime.registerFunction("doubleOrNull", 1, (context) -> Coerce.asDouble(context.getArgument(0)).orElse(null));
+
         runtime.registerFunction("string", 1, (context) -> context.getString(0));
         runtime.registerFunction("array", 1, (context) -> {
             Collection<?> collection = context.getArgumentByType(0, Collection.class);
